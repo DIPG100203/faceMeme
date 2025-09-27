50b4dfa75b68b09b1e87d5e2e8b4c60962727a3d
Daniel Ivan Guerrero Ponce <danielivanguerreroponce@gamil.com>
2025-09-26 23:10:19 -0600

    vista inicio de sesion y regitro UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InicioSesionComponent } from "./inicio-sesion/inicio-sesion.component";
+import { RegistroComponent } from "./registro/registro.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InicioSesionComponent, RegistroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
