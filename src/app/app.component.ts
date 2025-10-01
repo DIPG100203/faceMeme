f0550cc4c6305748b0e1f1f74f6cbb2af0435684
Daniel Ivan Guerrero Ponce <danielivanguerreroponce@gamil.com>
2025-09-30 23:40:03 -0600

    inicio de feed -Ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InicioSesionComponent } from "./inicio-sesion/inicio-sesion.component";
 import { RegistroComponent } from "./registro/registro.component";
+import { FeedComponent } from "./feed/feed.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InicioSesionComponent, RegistroComponent],
+  imports: [RouterOutlet, InicioSesionComponent, RegistroComponent, FeedComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/feed/feed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feed',
+  standalone: true,
+  imports: [],
+  templateUrl: './feed.component.html',
+  styles: ``
+})
+export class FeedComponent {
+
+}
