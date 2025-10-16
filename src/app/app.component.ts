51ef7219582b866e9f2ed33de9d25eb701166804
Daniel Ivan Guerrero Ponce <danielivanguerreroponce@gamil.com>
2025-10-16 16:35:39 -0600

    Actualizar componente de perfil: agregar modal para crear nueva publicación y ajustar imports en app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { InicioSesionComponent } from "./inicio-sesion/inicio-sesion.component";
 import { RegistroComponent } from "./registro/registro.component";
 import { FeedComponent } from "./feed/feed.component";
+import { PerfilComponent } from "./perfil/perfil.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InicioSesionComponent, RegistroComponent, FeedComponent],
+  imports: [RouterOutlet, InicioSesionComponent, RegistroComponent, FeedComponent, PerfilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
